82fc9d373db1ce404a524ea843271a510d132729
MatheusNachtigall <matheusgnachtigall@gmail.com>
2023-04-27 19:59:45 -0300

    integracao sql-node

--- docker/node/index.js
+++ docker/node/index.js
@@ -2,6 +2,21 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+//config sql
+const config = {
+    host: 'db', //nome do container
+    user: 'root',
+    password: 'root',
+    database: 'nodedb'
+}
+
+const mysql = require('mysql')
+const connection = mysql.createConnection(config)
+
+const sql = `INSERT INTO people(name) VALUES('Matheus');`;
+connection.query(sql);
+connection.end();
+
 app.get('/', (req,res) => {
     res.send('<h1>Matheus</h1>')
 })
