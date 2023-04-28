48fc629873e2db7d380fb386c909806eaf5f0bdd
MatheusNachtigall <matheusgnachtigall@gmail.com>
2023-04-28 16:04:15 -0300

    Desafio 2 - docker

--- docker/desafio_2/node/index.js
+++ docker/desafio_2/node/index.js
@@ -11,26 +11,38 @@ const config = {
     database: 'nodedb'
 }
 
-// const mysql = require('mysql')
-// const connection = mysql.createConnection(config)
+const mysql = require('mysql')
 
-// const createTable = `create table IF NOT EXISTS people(id int not null auto_increment, name varchar(255), primary key(id))`;
-// connection.query(createTable);
-
-// const insertIntoTable = `INSERT INTO people(name) VALUES('Matheus');`;
-// connection.query(insertIntoTable);
-
-// connection.end();
 
 async function getFakeName() {
     let res = await axios.get('https://api.namefake.com/');
-    let name = res.data.name;
-    console.log(names);
+    return res.data.name;
 }
 
-app.get('/', (req,res) => {
-    res.send('<h1>Full Cycle Rocks2!!</h1>')
-    getFakeName();
+app.get('/', async (req,res) => {
+    let name = await getFakeName();
+
+    const connection = mysql.createConnection(config)
+
+    const createTable = `create table IF NOT EXISTS people(id int not null auto_increment, name varchar(255), primary key(id))`;
+    connection.query(createTable);
+
+    let sqlQuery = `INSERT INTO people(name) VALUES('${name}');`;
+    connection.query(sqlQuery);
+
+    sqlQuery = `SELECT name FROM people;`;
+    connection.query(sqlQuery, (err, result, fields) => {
+        let table = '<table>';
+        table += '<tr><th>Nomes na tabela:</th></tr>';
+        for(let person of result) {      
+            table += `<tr><td>${person.name}</td></tr>`;
+        }
+        table += '</table>';
+        res.send('<h1>Full Cycle Rocks2!!</h1>' + table)
+    });
+
+    connection.end();
+
 })
 
 app.listen(port, () => {
